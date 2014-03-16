35e7fe6a4a141749bb756828f1a608dd55ffafb6
ofirventura <ofirventura@gmail.com>
2014-03-16 21:10:01 +0200

    seam removal

--- SeamCarving/src/Main.java
+++ SeamCarving/src/Main.java
@@ -9,35 +9,39 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		String input = args[0];
+		int newHeight = Integer.parseInt(args[1]);
+		int newWidth =  Integer.parseInt(args[2]);
 		BufferedImage img = ImageIO.read(new File(input));
-		SeamCarving s = new SeamCarving(img);
-		int[][] a = s.calcMapEntropy();
+		SeamCarving s = new SeamCarving(img, newWidth, newHeight);
+		//double[][] a = s.calcMapEntropy();
+		/*
 		int n = a.length;
 		int m = a[a.length-1].length;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				//System.out.print(a[i][j] + " ");
-				img.setRGB(i, j, (a[i][j]) | (a[i][j] << 8) | (a[i][j] << 16));
+				img.setRGB(i, j, ((int)a[i][j]) | ((int)a[i][j] << 8) | ((int)a[i][j] << 16));
 			}
 			//System.out.println("\n");
 		}
 		File outputfile = new File("image_entropy.jpg");
 		ImageIO.write(img, "jpg", outputfile);
-		
+		*/
 		BufferedImage img2 = ImageIO.read(new File(input));
-		SeamCarving s2 = new SeamCarving(img2);
-		int[][] b = s2.calcMapGradient();
-		int k = a.length;
-		int l = a[a.length-1].length;
+		SeamCarving s2 = new SeamCarving(img2,newWidth, newHeight);
+		//double[][] b = s2.calcMapGradient();
+		s2.Seam();
+		/*int k = b.length;
+		int l = b[b.length-1].length;
 		for (int i = 0; i < k; i++) {
 			for (int j = 0; j < l; j++) {
 				//System.out.print(a[i][j] + " ");
-				img2.setRGB(i, j, (b[i][j]) | (b[i][j] << 8) | (b[i][j] << 16));
+				img2.setRGB(i, j, ((int)b[i][j]) | ((int)b[i][j] << 8) | ((int)b[i][j] << 16));
 			}
 			//System.out.println("\n");
 		}
 		File outputfile2 = new File("image_gradiant.jpg");
-		ImageIO.write(img2, "jpg", outputfile2);
+		ImageIO.write(img2, "jpg", outputfile2);*/
 	}
 	
 }
